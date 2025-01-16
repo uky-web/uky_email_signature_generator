265112fddb1d1e560cc338d69d946e0d61dcd0c2
Ren Fox <ren.fox@uky.edu>
2025-01-16 13:25:18 -0500

    Add plaintext signature
    Module now generates a plaintext alternative with form fields, copy
    button copies both versions to the clipboard which should correctly be
    pasted into editors that do or don't support HTML/rich text.
    
    Refs: #5, #6

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 (function ($, Drupal) {
-  function copyToClipboard(html) {
+  function copyToClipboard(html, text = null) {
+    // Generate an element with the HTML content
     const el = document.createElement('div');
     el.innerHTML = html;
-    const plain = el.textContent || html;
+    // Set text content if provided, otherwise infer/generate from HTML
+    const plain = text || el.textContent || html;
+    const htmlBlob = new Blob([el.innerHTML], {type: 'text/html'});
+    const textBlob = new Blob([plain], {type: 'text/plain'});
 
-    const content = el.innerHTML;
-    const blob = new Blob([content], {type: 'text/html'});
-    const text = new Blob([plain], {type: 'text/plain'});
+    // Create clipboard item and write to clipboard
     const clipboardItem = new window.ClipboardItem({
-      'text/html': blob ,
-      'text/plain': text
+      'text/html': htmlBlob ,
+      'text/plain': textBlob
     });
     return navigator.clipboard.write([clipboardItem]);
   }
 
   $('*[data-action="copy-element"]').click(function(e) {
     const elem = $(this);
-    const target = $(this).data('target');
-    copyToClipboard($(target).html()).then(function(result) {
+    const htmlTarget = $(this).data('target');
+    const textTarget = $(this).data('target-text') || null;
+
+    // Copy element(s) and update button text
+    copyToClipboard($(htmlTarget).html(), $(textTarget).text() || null).then(function(result) {
       elem.text('Copied!');
     }, function(err) {
-      elem.text('Copy to clipboard failed');
+      elem.text('Copy failed');
     });
   });
 })(jQuery, Drupal);
